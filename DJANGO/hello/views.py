b76a5e862bb22f22bca4541299f8bec92f3f4d68
ArunimaShukla03 <arunimashukla2003@gmail.com>
2022-12-28 14:53:54 +0530

    templates in django

--- DJANGO/hello/views.py
+++ DJANGO/hello/views.py
@@ -4,11 +4,22 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hello World!")
+  return render(request, "hello/index.html")
 
 
 def arunima(request):
   return HttpResponse("Hello Arunima!")
 
+'''
+
 def greet(request, name):
   return HttpResponse(f"Hello, {name.capitalize()}!")
+  
+'''
+
+# A second way to write a greet function that does the same job while separating HTML is by the following way: 
+
+def greet(request, name):
+  return render(request,"hello/greet.html", {
+    "name": name.capitalize()
+  })
