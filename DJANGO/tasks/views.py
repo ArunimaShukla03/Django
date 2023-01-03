af9f5d093b4ac1dfce8725a4f4d0100c975f296d
ArunimaShukla03 <arunimashukla2003@gmail.com>
2023-01-03 13:24:05 +0530

    forms in django

--- DJANGO/tasks/views.py
+++ DJANGO/tasks/views.py
@@ -1,7 +1,12 @@
+from django import forms
 from django.shortcuts import render
 
 task = ["foo", "baz", "lol"]
 # Create your views here.
+
+class NewTaskForm(forms.Form):
+  tasksss = forms.CharField(label="New Task:")
+  priority = forms.IntegerField(label="Priority", min_value=1)
 
 def index(request):
   return render(request, "tasks/index.html", {
@@ -9,4 +14,18 @@ def index(request):
   })
   
 def add(request):
-  return render(request, "tasks/add.html")
+  if request.method == "POST":
+    form = NewTaskForm(request.POST)
+
+    if form.is_valid():
+      tasksss = form.cleaned_data["tasksss"]
+      task.append(tasksss)
+
+    else:
+      return render(request, "tasks/add.html", {
+        "forms":form
+      })
+
+  return render(request, "tasks/add.html", {
+    "forms":NewTaskForm
+  })
